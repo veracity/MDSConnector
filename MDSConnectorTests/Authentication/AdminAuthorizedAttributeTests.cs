c49d3941dc66c5984aeda68c89c601eb0009ca11
HaiyueCHen <chenhaiyue2010@hotmail.com>
2020-03-21 17:13:08 +0100

    unit test for filter attributes

--- MDSConnectorTests/Authentication/AdminAuthorizedAttributeTests.cs
+++ MDSConnectorTests/Authentication/AdminAuthorizedAttributeTests.cs
@@ -3,6 +3,13 @@ using MDSConnector.Authentication;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Moq;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.AspNetCore.Mvc.Abstractions;
 
 namespace MDSConnector.Authentication.Tests
 {
@@ -10,9 +17,242 @@ namespace MDSConnector.Authentication.Tests
     public class AdminAuthorizedAttributeTests
     {
         [TestMethod()]
-        public void OnAuthorizationTest()
+        public void UnauthorizedResult_UserNotAuthenticated()
         {
-            Assert.Fail();
+            // Arrange
+            var adminAuthorizedAttribute = new AdminAuthorizedAttribute();
+            var mockHttpContext = new Mock<HttpContext>();
+
+            List<Claim> claims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.AuthenticationMethod, "Certi1ficate"),
+                new Claim(ClaimTypes.Role, "User"),
+                new Claim(CertificateClaimTypes.Subject, "root_ca_dnvgl_dev.com"),
+                new Claim(CertificateClaimTypes.Issuer, "root_ca_dnvgl_dev.com"),
+                new Claim(CertificateClaimTypes.Thumbprint, "some thumbprint, not important for this test")
+            };
+
+            var user = new ClaimsPrincipal(new ClaimsIdentity(claims));
+
+            mockHttpContext.SetupGet(h => h.User).Returns(user);
+            
+            var actionContext = new ActionContext(mockHttpContext.Object, new RouteData(), new ActionDescriptor());
+            var authorizationFilterContext = new AuthorizationFilterContext(actionContext, new List<IFilterMetadata>());
+            // Act
+            adminAuthorizedAttribute.OnAuthorization(authorizationFilterContext);
+
+            // Assert
+            Assert.IsInstanceOfType(authorizationFilterContext.Result, typeof(UnauthorizedResult));
         }
+
+        [TestMethod()]
+        public void ForbidResult_UserNoAuthenticationMethod()
+        {
+            // Arrange
+            var adminAuthorizedAttribute = new AdminAuthorizedAttribute();
+            var mockHttpContext = new Mock<HttpContext>();
+
+            List<Claim> claims = new List<Claim>() { 
+                new Claim(ClaimTypes.Role, "Admin"),
+                new Claim(CertificateClaimTypes.Subject, "root_ca_dnvgl_dev.com"),
+                new Claim(CertificateClaimTypes.Issuer, "root_ca_dnvgl_dev.com"),
+                new Claim(ClaimTypes.Thumbprint, "some thumbprint, not important for this test"),
+            };
+            var user = new ClaimsPrincipal(new ClaimsIdentity(claims, "certificate"));
+            mockHttpContext.SetupGet(h => h.User).Returns(user);
+
+            var actionContext = new ActionContext(mockHttpContext.Object, new RouteData(), new ActionDescriptor());
+            var authorizationFilterContext = new AuthorizationFilterContext(actionContext, new List<IFilterMetadata>());
+            // Act
+            adminAuthorizedAttribute.OnAuthorization(authorizationFilterContext);
+
+            // Assert
+            Assert.IsInstanceOfType(authorizationFilterContext.Result, typeof(ForbidResult));
+        }
+
+        [TestMethod()]
+        public void ForbidResult_UserWrongAuthenticationMethod()
+        {
+            // Arrange
+            var adminAuthorizedAttribute = new AdminAuthorizedAttribute();
+            var mockHttpContext = new Mock<HttpContext>();
+
+            List<Claim> claims = new List<Claim>() {
+                new Claim(ClaimTypes.AuthenticationMethod, "oauth2"),
+                new Claim(ClaimTypes.Role, "Admin"),
+                new Claim(CertificateClaimTypes.Subject, "root_ca_dnvgl_dev.com"),
+                new Claim(CertificateClaimTypes.Issuer, "root_ca_dnvgl_dev.com"),
+                new Claim(ClaimTypes.Thumbprint, "some thumbprint, not important for this test"),
+            };
+            var user = new ClaimsPrincipal(new ClaimsIdentity(claims, "certificate"));
+            mockHttpContext.SetupGet(h => h.User).Returns(user);
+
+            var actionContext = new ActionContext(mockHttpContext.Object, new RouteData(), new ActionDescriptor());
+            var authorizationFilterContext = new AuthorizationFilterContext(actionContext, new List<IFilterMetadata>());
+            // Act
+            adminAuthorizedAttribute.OnAuthorization(authorizationFilterContext);
+
+            // Assert
+            Assert.IsInstanceOfType(authorizationFilterContext.Result, typeof(ForbidResult));
+        }
+
+        [TestMethod()]
+        public void ForbidResult_UserNoRole()
+        {
+            // Arrange
+            var adminAuthorizedAttribute = new AdminAuthorizedAttribute();
+            var mockHttpContext = new Mock<HttpContext>();
+
+            List<Claim> claims = new List<Claim>() {
+                new Claim(ClaimTypes.AuthenticationMethod, "Certificate"),
+                new Claim(CertificateClaimTypes.Subject, "root_ca_dnvgl_dev.com"),
+                new Claim(CertificateClaimTypes.Issuer, "root_ca_dnvgl_dev.com"),
+                new Claim(ClaimTypes.Thumbprint, "some thumbprint, not important for this test"),
+            };
+
+            var user = new ClaimsPrincipal(new ClaimsIdentity(claims, "certificate"));
+            mockHttpContext.SetupGet(h => h.User).Returns(user);
+            var actionContext = new ActionContext(mockHttpContext.Object, new RouteData(), new ActionDescriptor());
+            var authorizationFilterContext = new AuthorizationFilterContext(actionContext, new List<IFilterMetadata>());
+            // Act
+            adminAuthorizedAttribute.OnAuthorization(authorizationFilterContext);
+
+            // Assert
+            Assert.IsInstanceOfType(authorizationFilterContext.Result, typeof(ForbidResult));
+        }
+
+        [TestMethod()]
+        public void ForbidResult_UserNoSubject()
+        {
+            // Arrange
+            var adminAuthorizedAttribute = new AdminAuthorizedAttribute();
+            var mockHttpContext = new Mock<HttpContext>();
+            var mockUser = new Mock<ClaimsPrincipal>();
+
+            mockUser.SetupGet(u => u.Identity.IsAuthenticated).Returns(true);
+            Claim[] claims = new Claim[] {
+                new Claim(ClaimTypes.AuthenticationMethod, "Certificate"),
+                new Claim(ClaimTypes.Role, "Admin"),
+                new Claim(CertificateClaimTypes.Issuer, "root_ca_dnvgl_dev.com"),
+                new Claim(ClaimTypes.Thumbprint, "some thumbprint, not important for this test"),
+            };
+            mockUser.Setup(u => u.Claims).Returns(claims);
+            mockHttpContext.SetupGet(h => h.User).Returns(mockUser.Object);
+
+            var actionContext = new ActionContext(mockHttpContext.Object, new RouteData(), new ActionDescriptor());
+            var authorizationFilterContext = new AuthorizationFilterContext(actionContext, new List<IFilterMetadata>());
+            // Act
+            adminAuthorizedAttribute.OnAuthorization(authorizationFilterContext);
+
+            // Assert
+            Assert.IsInstanceOfType(authorizationFilterContext.Result, typeof(ForbidResult));
+        }
+
+        [TestMethod()]
+        public void ForbidResult_UserNoIssuer()
+        {
+            // Arrange
+            var adminAuthorizedAttribute = new AdminAuthorizedAttribute();
+            var mockHttpContext = new Mock<HttpContext>();
+            var mockUser = new Mock<ClaimsPrincipal>();
+
+            mockUser.SetupGet(u => u.Identity.IsAuthenticated).Returns(true);
+            Claim[] claims = new Claim[] {
+                new Claim(ClaimTypes.AuthenticationMethod, "Certificate"),
+                new Claim(ClaimTypes.Role, "Admin"),
+                new Claim(CertificateClaimTypes.Subject, "root_ca_dnvgl_dev.com"),
+                new Claim(ClaimTypes.Thumbprint, "some thumbprint, not important for this test"),
+            };
+            mockUser.Setup(u => u.Claims).Returns(claims);
+            mockHttpContext.SetupGet(h => h.User).Returns(mockUser.Object);
+
+            var actionContext = new ActionContext(mockHttpContext.Object, new RouteData(), new ActionDescriptor());
+            var authorizationFilterContext = new AuthorizationFilterContext(actionContext, new List<IFilterMetadata>());
+            // Act
+            adminAuthorizedAttribute.OnAuthorization(authorizationFilterContext);
+
+            // Assert
+            Assert.IsInstanceOfType(authorizationFilterContext.Result, typeof(ForbidResult));
+        }
+
+        [TestMethod()]
+        public void ForbidResult_UserNoThumbprint()
+        {
+            // Arrange
+            var adminAuthorizedAttribute = new AdminAuthorizedAttribute();
+            var mockHttpContext = new Mock<HttpContext>();
+
+            List<Claim> claims = new List<Claim>() {
+                new Claim(ClaimTypes.AuthenticationMethod, "Certificate"),
+                new Claim(ClaimTypes.Role, "Admin"),
+                new Claim(CertificateClaimTypes.Subject, "root_ca_dnvgl_dev.com"),
+                new Claim(CertificateClaimTypes.Issuer, "root_ca_dnvgl_dev.com"),
+            };
+            var user = new ClaimsPrincipal(new ClaimsIdentity(claims, "certificate"));
+            mockHttpContext.SetupGet(h => h.User).Returns(user);
+
+            var actionContext = new ActionContext(mockHttpContext.Object, new RouteData(), new ActionDescriptor());
+            var authorizationFilterContext = new AuthorizationFilterContext(actionContext, new List<IFilterMetadata>());
+            // Act
+            adminAuthorizedAttribute.OnAuthorization(authorizationFilterContext);
+
+            // Assert
+            Assert.IsInstanceOfType(authorizationFilterContext.Result, typeof(ForbidResult));
+        }
+
+        [TestMethod()]
+        public void ForbidResult_UserNotAdmin()
+        {
+            // Arrange
+            var adminAuthorizedAttribute = new AdminAuthorizedAttribute();
+            var mockHttpContext = new Mock<HttpContext>();
+
+            List<Claim> claims = new List<Claim>() {
+                new Claim(ClaimTypes.AuthenticationMethod, "Certificate"),
+                new Claim(ClaimTypes.Role, "User"),
+                new Claim(CertificateClaimTypes.Subject, "root_ca_dnvgl_dev.com"),
+                new Claim(CertificateClaimTypes.Issuer, "root_ca_dnvgl_dev.com"),
+                new Claim(CertificateClaimTypes.Thumbprint, "some thumbprint, not important for this test")
+            };
+            var user = new ClaimsPrincipal(new ClaimsIdentity(claims, "certificate"));
+            mockHttpContext.SetupGet(h => h.User).Returns(user);
+
+            var actionContext = new ActionContext(mockHttpContext.Object, new RouteData(), new ActionDescriptor());
+            var authorizationFilterContext = new AuthorizationFilterContext(actionContext, new List<IFilterMetadata>());
+            
+            // Act
+            adminAuthorizedAttribute.OnAuthorization(authorizationFilterContext);
+
+            // Assert
+            Assert.IsInstanceOfType(authorizationFilterContext.Result, typeof(ForbidResult));
+        }
+
+        [TestMethod()]
+        public void NoResult_UserCorrect()
+        {
+            // Arrange
+            var adminAuthorizedAttribute = new AdminAuthorizedAttribute();
+            var mockHttpContext = new Mock<HttpContext>();
+            List<Claim> claims = new List<Claim>() {
+                new Claim(ClaimTypes.AuthenticationMethod, "Certificate"),
+                new Claim(ClaimTypes.Role, "Admin"),
+                new Claim(CertificateClaimTypes.Subject, "root_ca_dnvgl_dev.com"),
+                new Claim(CertificateClaimTypes.Issuer, "root_ca_dnvgl_dev.com"),
+                new Claim(CertificateClaimTypes.Thumbprint, "some thumbprint, not important for this test")
+            };
+            var user = new ClaimsPrincipal(new ClaimsIdentity(claims, "certificate"));
+            mockHttpContext.SetupGet(h => h.User).Returns(user);
+            
+            var actionContext = new ActionContext(mockHttpContext.Object, new RouteData(), new ActionDescriptor());
+            var authorizationFilterContext = new AuthorizationFilterContext(actionContext, new List<IFilterMetadata>());
+            
+            // Act
+            adminAuthorizedAttribute.OnAuthorization(authorizationFilterContext);
+
+            // Assert
+            Assert.IsNull(authorizationFilterContext.Result);
+        }
+
+
     }
 }
--- MDSConnectorTests/Authentication/CustomCertificateAuthenticatorTests.cs
+++ MDSConnectorTests/Authentication/CustomCertificateAuthenticatorTests.cs
@@ -3,16 +3,69 @@ using MDSConnector.Authentication;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Moq;
+using Microsoft.Extensions.Options;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Logging;
+using System.Text.Encodings.Web;
+using MDSConnector.Utilities.ConfigHelpers;
+using Microsoft.AspNetCore.Http;
+using System.Threading;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
 
 namespace MDSConnector.Authentication.Tests
 {
     [TestClass()]
     public class CustomCertificateAuthenticatorTests
     {
+
+        private Mock<IOptionsMonitor<AuthenticationSchemeOptions>> _options;
+        private Mock<ILoggerFactory> _loggerFactory;
+        private Mock<UrlEncoder> _encoder;
+        private Mock<ISystemClock> _clock;
+        private CustomCertificateAuthenticator _handler;
+
+
         [TestMethod()]
-        public void CustomCertificateAuthenticatorTest()
+        public async Task NoCertificate_defaultClaims()
         {
-            Assert.Fail();
+            // Arrange
+            _options = new Mock<IOptionsMonitor<AuthenticationSchemeOptions>>();
+
+            var logger = new Mock<ILogger<CustomCertificateAuthenticator>>();
+            _loggerFactory = new Mock<ILoggerFactory>();
+            _loggerFactory.Setup(x => x.CreateLogger(It.IsAny<String>())).Returns(logger.Object);
+
+            _encoder = new Mock<UrlEncoder>();
+            _clock = new Mock<ISystemClock>();
+
+            var validIssuers = new string[] { "CN=root_ca_dnvgl_dev.com", "CN=root_ca_microsoft_dev.com" };
+            var knownIssuers = new KnownCertificateIssuers();
+            knownIssuers.validIssuers = validIssuers;
+            var knownIssuersOptions = new Mock<IOptions<KnownCertificateIssuers>>();
+            knownIssuersOptions.Setup(x => x.Value).Returns(knownIssuers);
+            _handler = new CustomCertificateAuthenticator(_options.Object, _loggerFactory.Object, _encoder.Object, _clock.Object, knownIssuersOptions.Object);
+
+
+            var mockHttpContext = new Mock<HttpContext>();
+            var mockConenctionInfo = new Mock<ConnectionInfo>();
+            mockConenctionInfo.Setup(c => c.GetClientCertificateAsync(default(CancellationToken)))
+                .Returns(Task.FromResult<X509Certificate2>(null));
+            
+            mockHttpContext.Setup(c => c.Connection)
+                .Returns(mockConenctionInfo.Object);
+
+            // Act
+            var authenticationScheme = new AuthenticationScheme("CustomCertificateAuthentication", "CustomCertificateAuthentication", typeof(CustomCertificateAuthenticator));
+            await _handler.InitializeAsync(authenticationScheme, mockHttpContext.Object);
+            var result = await _handler.AuthenticateAsync();
+
+            // Assert
+            Assert.IsTrue(result.Succeeded);
+            Assert.AreEqual(_handler.Scheme.Name, result.Ticket.AuthenticationScheme);
+            Assert.AreEqual("default", result.Ticket.Principal.Identity.Name);
+
         }
     }
 }
